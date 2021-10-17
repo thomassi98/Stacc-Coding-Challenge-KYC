1fb0279e8614b01fd58f7bb6eceb3d85720c16f5
Thomas Solberg Irgens <thomastsi98@gmail.com>
2021-10-17 12:19:14 +0200

    Implements KYC_API REST API app with Stacc_API data

--- KYC_API.py
+++ KYC_API.py
@@ -1,10 +1,24 @@
 from flask import Flask, jsonify
+from KYCSearch import Stacc_API
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Welcome to the KYC API"
+
+@app.route('/PEP/name=<string:name>', methods = ['GET'])
+def get_PEP(name):
+    return Stacc_API.get_PEP(name)
+
+@app.route('/roles/org_num=<string:org_num>', methods = ['GET'])
+def get_roles(org_num):
+    return Stacc_API.get_roles(org_num)
+
+@app.route('/company/org_num=<string:org_num>', methods = ['GET'])
+def get_company(org_num):
+    return Stacc_API.get_company(org_num)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
